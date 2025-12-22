1af18bd386f9f0f4594af7e88bd3e509f9641c3c
SlimMicheals <slimmicheals77@gmail.com>
2025-12-22 23:09:26 +0100

    Implement getAngleType and set up Jest

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -1,7 +1,8 @@
 function getAngleType(angle) {
+  if (angle < 90) return "Acute angle";
   if (angle === 90) return "Right angle";
-  // replace with your completed function from key-implement
-
+  if (angle > 90 && angle < 180) return "Obtuse angle";
+  if (angle === 180) return "Straight angle";
 }
 
 
